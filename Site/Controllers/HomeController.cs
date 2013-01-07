956e10759b37f51f5b866b6703b0a605a92e8995
WW13 <wjwater@gmail.com>
2013-01-07 22:22:04 +0100

    fixed test

--- Site/Controllers/HomeController.cs
+++ Site/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Site.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template - to break the test - to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
         }
